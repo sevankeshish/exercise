5870a659e05eaa02b987507dc769747779fb2c54
Sevan Keshish <35175803+sevankeshish@users.noreply.github.com>
2022-02-18 10:26:59 +0330

    Fragment

--- src/components/HooksUpdateArray/hooksUpdateArray.jsx
+++ src/components/HooksUpdateArray/hooksUpdateArray.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 
 export const HooksUpdateArray = () => {
   const [item, setItem] = useState([]);
 
   const addItemHandler = () => {
+    /////////////////raveshe aval/////////////
     // setItem([
     //   ...item,
     //   { id: item.length, number: Math.floor(Math.random() * 10) },
@@ -12,17 +13,20 @@ export const HooksUpdateArray = () => {
       id: item.length,
       number: Math.floor(Math.random() * 10),
     };
+    // console.log({ addedItem }, "hi");
 
     const updatedItem = [...item, addedItem];
     setItem(updatedItem);
   };
 
   return (
+    // <Fragment>
     <div>
       <button onClick={addItemHandler}>add items</button>
       {item.map((e) => {
         return <li key={e.id}>{e.number}</li>;
       })}
     </div>
+    // </Fragment>
   );
 };
